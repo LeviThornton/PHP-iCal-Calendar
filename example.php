a4892b69f86890037430848c841c10ace1b3aa5c
Levi Thornton <LeviThorntonXC@Gmail.com>
2014-01-12 16:55:27 -0500

    Cleaned up a little

--- example.php
+++ example.php
@@ -1,7 +1,6 @@
 <?php
 require_once("Model_iCal.php");
 	$events = array();
-
 	$events[] = $iCalEvent = new IcalEvent();
 	        $iCalEvent->setSummery("Christmas");
 	        $iCalEvent->setUid("500012345");
@@ -16,7 +15,6 @@ require_once("Model_iCal.php");
 	        $iCalEvent->setDtstart('20070311T100000');
 	        //$iCalEvent->setDtend(''); // end time
 	        $iCalEvent->setLocation("New York, NY");
-
 	$ical = new Application_Model_Ical($events);
 	$ical->printiCal();
 ?>
